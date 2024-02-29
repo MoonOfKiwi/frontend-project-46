45e7cc1767e5086175e81f14fb5948681527f010
Anastasia_Surkova <existence117@gmail.com>
2024-02-29 20:34:19 +0300

    refactor: add version instead option(version)

--- src/gendiffInfo.js
+++ src/gendiffInfo.js
@@ -3,8 +3,8 @@ import { program } from 'commander';
 const getGendiffInfo = () => {
   program
     .name('gendiff')
+    .version('')
     .description('Compares two configuration files and shows a difference.')
-    .option('-V, --version', 'output the version number')
     .helpOption('-h, --help', 'output usage information');
 
   program.parse();
