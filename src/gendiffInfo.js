ee83951ae2c68b66508ca208d711028eb1a83ce2
Anastasia_Surkova <existence117@gmail.com>
2024-03-05 18:39:15 +0300

    feat: add arguments and format-option

--- src/gendiffInfo.js
+++ src/gendiffInfo.js
@@ -3,8 +3,11 @@ import { program } from 'commander';
 const getGendiffInfo = () => {
   program
     .name('gendiff')
-    .version('')
+    .version('1.0.0')
     .description('Compares two configuration files and shows a difference.')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
+    .option('-f, --format [type]', 'output format')
     .helpOption('-h, --help', 'output usage information');
 
   program.parse();
