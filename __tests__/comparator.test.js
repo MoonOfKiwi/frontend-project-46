3c731af2cbaf85cf7886fa029b6ee999c515cb04
Anastasia_Surkova <existence117@gmail.com>
2024-03-28 22:08:04 +0300

    test: remove link to parser, add compareFileData tests

--- __tests__/comparator.test.js
+++ __tests__/comparator.test.js
@@ -1,20 +1,20 @@
 import _ from 'lodash';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
+import fs from 'fs';
 import {
   getCompareReducer,
   SAME, FIRST_EXISTS, SECOND_EXISTS, DIFFERENT,
   compareFileData,
 } from '../src/comparator.js';
-import parseFile from '../src/parser.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => join(__dirname, '__fixtures__', filename);
 
-const file1 = parseFile(getFixturePath('file1.json'));
-const file2 = parseFile(getFixturePath('file2.json'));
+const file1 = JSON.parse(fs.readFileSync(getFixturePath('file1.json')));
+const file2 = JSON.parse(fs.readFileSync(getFixturePath('file2.json')));
 
 test('getCompareReducer returns reducer function', () => {
   expect(typeof getCompareReducer(file1, file2)).toBe('function');
@@ -39,4 +39,11 @@ test('Reducer testing', () => {
 test('compareFileData tests', () => {
   const result = compareFileData(file1, file2);
   expect(Object.keys(result)).toEqual(_.sortBy(Object.keys(result)));
+  expect(result).toEqual({
+    follow: { file1: false, file2: undefined, diff: FIRST_EXISTS },
+    host: { file1: 'hexlet.io', file2: 'hexlet.io', diff: SAME },
+    proxy: { file1: '123.234.53.22', file2: undefined, diff: FIRST_EXISTS },
+    timeout: { file1: 50, file2: 20, diff: DIFFERENT }, 
+    verbose: { file1: undefined, file2: true, diff: SECOND_EXISTS}
+  })
 });
