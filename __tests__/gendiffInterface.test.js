79d28183b489ea529fdc471bcd3fd554d959ce48
Anastasia_Surkova <existence117@gmail.com>
2024-04-16 19:21:17 +0300

    test: add tests for checking plain-format, json-format

--- __tests__/gendiffInterface.test.js
+++ __tests__/gendiffInterface.test.js
@@ -23,3 +23,33 @@ test('makeGendiff: type - yaml, format - stylish', () => {
   const result = makeGendiff(filepath1, filepath2);
   expect(result).toEqual(answer);
 });
+
+test('makeGendiff: type - json, format - plain', () => {
+  const filepath1 = getFixturePath('treefile1.json');
+  const filepath2 = getFixturePath('treefile2.json');
+  const answer = fs.readFileSync(getFixturePath('treefilePlainExpected.txt'), 'utf-8');
+  const result = makeGendiff(filepath1, filepath2, 'plain');
+  expect(result).toEqual(answer);
+});
+
+test('makeGendiff: type - yaml, format - plain', () => {
+  const filepath1 = getFixturePath('treefile1.yaml');
+  const filepath2 = getFixturePath('treefile2.yaml');
+  const answer = fs.readFileSync(getFixturePath('treefilePlainExpected.txt'), 'utf-8');
+  const result = makeGendiff(filepath1, filepath2, 'plain');
+  expect(result).toEqual(answer);
+});
+
+test('makeGendiff: type - json, format - json', () => {
+  const filepath1 = getFixturePath('treefile1.json');
+  const filepath2 = getFixturePath('treefile2.json');
+  const answer = fs.readFileSync(getFixturePath('treefileJSONExpected.json'), 'utf-8');
+  const result = makeGendiff(filepath1, filepath2, 'json');
+  expect(result).toEqual(answer);
+});
+
+test('makeGendiff: type - json, format - notCorrectFormatter', () => {
+  const filepath1 = getFixturePath('treefile1.json');
+  const filepath2 = getFixturePath('treefile2.json');
+  expect(() => makeGendiff(filepath1, filepath2, 'abrakadabra')).toThrow('Format abrakadabra is not supported!');
+});
