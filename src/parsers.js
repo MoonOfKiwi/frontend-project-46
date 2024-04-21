d9aee618a6d377ce67a427f1ae1c2f5a2b9966b4
Anastasia_Surkova <existence117@gmail.com>
2024-04-21 22:28:54 +0300

    refactor: moved file-reading functions to src/fileUtils.js

--- src/parsers.js
+++ src/parsers.js
@@ -1,24 +1,5 @@
-import fs from 'fs';
-import path from 'path';
 import yaml from 'yaml';
-
-const formatFilepath = (filepath) => {
-  if (filepath.startsWith('/')) {
-    return filepath;
-  }
-
-  const cwd = process.cwd();
-  const fullPath = path.resolve(cwd, filepath);
-  return fullPath;
-};
-
-const readFile = (filepath) => {
-  const formattedFilepath = formatFilepath(filepath);
-  const data = fs.readFileSync(formattedFilepath, 'utf-8');
-  return data;
-};
-
-const getFileExtension = (filepath) => path.extname(filepath);
+import { formatFilepath, readFile, getFileExtension } from './fileUtils.js';
 
 const parseData = (fileformat, filedata) => {
   switch (fileformat) {
