59daf1c0c00cdc7c454c2688c9433eb830b7187c
Anastasia_Surkova <existence117@gmail.com>
2024-04-21 22:38:26 +0300

    fix: deleted func gendiffCLI, only CLI

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,20 +2,16 @@
 import { program } from 'commander';
 import gendiff from '../src/gendiff.js';
 
-const gendiffCLI = () => {
-  program
-    .name('gendiff')
-    .version('1.0.0')
-    .description('Compares two configuration files and shows a difference.')
-    .argument('<filepath1>')
-    .argument('<filepath2>')
-    .option('-f, --format <type>', 'output format', 'stylish')
-    .option('--colors')
-    .helpOption('-h, --help', 'output usage information')
-    .action((filepath1, filepath2, options) => {
-      console.log(gendiff(filepath1, filepath2, options.format));
-    })
-    .parse();
-};
-
-gendiffCLI();
+program
+  .name('gendiff')
+  .version('1.0.0')
+  .description('Compares two configuration files and shows a difference.')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .option('--colors')
+  .helpOption('-h, --help', 'output usage information')
+  .action((filepath1, filepath2, options) => {
+    console.log(gendiff(filepath1, filepath2, options.format));
+  })
+  .parse();
