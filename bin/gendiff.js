aa77f7ea717a35db099b6c6a48e810e2dac73203
Anastasia_Surkova <existence117@gmail.com>
2024-04-22 15:18:59 +0300

    refactor: add gendiffCLI

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,14 +2,19 @@
 import { program } from 'commander';
 import gendiff from '../src/gendiff.js';
 
-program
-  .name('gendiff')
-  .description('Compares two configuration files and shows a difference.')
-  .argument('<filepath1>')
-  .argument('<filepath2>')
-  .option('-f, --format [type]', 'output format', 'stylish')
-  .helpOption('-h, --help', 'output usage information')
-  .action((filepath1, filepath2, options) => {
-    console.log(gendiff(filepath1, filepath2, options.format));
-  })
-  .parse();
+const gendiffCLI = () => {
+  program
+    .name('gendiff')
+    .version('1.0.0')
+    .description('Compares two configuration files and shows a difference.')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
+    .option('-f, --format <type>', 'output format', 'stylish')
+    .helpOption('-h, --help', 'output usage information')
+    .action((filepath1, filepath2, options) => {
+      console.log(gendiff(filepath1, filepath2, options.format));
+    })
+    .parse();
+};
+
+gendiffCLI();
