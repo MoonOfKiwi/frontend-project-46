8bfbfed5e185a28de34957dea8e10f87622355e3
Anastasia_Surkova <existence117@gmail.com>
2024-04-25 23:17:02 +0300

    test: simplify gendiff tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -14,28 +14,20 @@ test('makeGendiff: Throws "Format is not supported!"', () => {
   expect(() => makeGendiff(filepath1, filepath2, 'abrakadabra')).toThrow('Format abrakadabra is not supported!');
 });
 
-test.each([
-  {
-    type: 'json', file1: 'file1.json', file2: 'file2.json', format: 'stylish', expected: 'expectedStylish.txt',
-  },
-  {
-    type: 'yaml', file1: 'file1.yaml', file2: 'file2.yaml', expected: 'expectedStylish.txt',
-  },
-  {
-    type: 'json', file1: 'file1.json', file2: 'file2.json', format: 'plain', expected: 'expectedPlain.txt',
-  },
-  {
-    type: 'yaml', file1: 'file1.yaml', file2: 'file2.yaml', format: 'plain', expected: 'expectedPlain.txt',
-  },
-  {
-    type: 'json', file1: 'file1.json', file2: 'file2.json', format: 'json', expected: 'expectedJSON.json',
-  },
-])('makeGendiff: $type, format $format', ({
-  file1, file2, format, expected,
-}) => {
-  const filepath1 = getFixturePath(file1);
-  const filepath2 = getFixturePath(file2);
-  const answer = fs.readFileSync(getFixturePath(expected), 'utf-8');
-  const result = makeGendiff(filepath1, filepath2, format);
-  expect(result).toEqual(answer);
+describe.each([
+  ['stylish', 'expectedStylish.txt'],
+  ['plain', 'expectedPlain.txt'],
+  ['json', 'expectedJSON.json'],
+])('%s formatter', (formatter, expectedFile) => {
+  const filepathOfExpected = getFixturePath(expectedFile);
+  const expected = fs.readFileSync(filepathOfExpected, 'utf-8');
+
+  test.each([['json'], ['yaml']])('%s files', (extension) => {
+    const filepath1 = getFixturePath(`file1.${extension}`);
+    const filepath2 = getFixturePath(`file2.${extension}`);
+
+    const result = makeGendiff(filepath1, filepath2, formatter);
+
+    expect(result).toBe(expected);
+  });
 });
